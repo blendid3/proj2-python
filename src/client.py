ce469be93aa7f3ac7641c8a7543c78ec16419b24
suyanzhe <suyanzhe970604@outlook.com>
2019-11-08 14:41:14 -0800

    split and hash, create and read out index.txt

--- src/client.py
+++ src/client.py
@@ -1,29 +1,27 @@
 import argparse
 import xmlrpc.client
 import os
-import sys
 import hashlib
 from pathlib import Path
+import shutil
 
 
 class Client:
     def __init__(self, args):
-        self.basedir = args.basedir
-        self.hostport = args.hostport
-        self.blocksize = args.blocksize
         self.client = None
         self.error = False
 
         try:
-            self.blocksize = int(self.blocksize)
-            self.hostport = str(self.hostport)
-            self.basedir = str(self.basedir)
-            client = xmlrpc.client.ServerProxy(self.hostport)
-            client.surfstore.ping()
-            print("Ping() successful")
+            self.blocksize = int(args.blocksize)
+            self.basedir = Path(str(args.basedir)).resolve(
+                strict=False)  # Requires Python 3.6
+            self.hostport = str(args.hostport)
+            self.client = xmlrpc.client.ServerProxy(self.hostport)
+            assert self.blocksize > 0
+
         except Exception as e:
             self.error = True
-            print("Client __init__: " + str(e))
+            print("Client __init__(): " + str(e))
 
     # chunk: bytes
     def hash_chunk(self, chunk):
@@ -33,54 +31,85 @@ class Client:
 
     # path: Path object according to https://docs.python.org/zh-cn/3/library/pathlib.html
     def get_file_size(self, path):
-        size = path.stat().st_size
-        print("getfilesize: size: %s" % size)
-        return size
-    
-    def sync(self):
-        files = os.listdir(self.basedir)  # 得到文件夹下的所有文件名称
-        for file in files:  # 遍历文件夹
-            if not os.path.isdir(file):  # 判断是否是文件夹，不是文件夹才打开
-                # 打开文件 the absolute path is path+file
-                f = open(self.basedir + "/" + file, 'rb')
-                buf = f.read()  # 读取所有内容
-                # 打印出该文件的名字，以及hash内容
-                print("{} ".format(os.path.basename(args.file)))
-
-    # path: Path object according to https://docs.python.org/zh-cn/3/library/pathlib.html
-    def split_file(self, path):
-        # Open original file in read only mode
-        # Do we need to consider symbolic links?
         if not path.is_file():
             print("No such file as: \"%s\"" % str(path))
             return
 
-        filesize = get_file_size(path)
-        filename = str(path)
-        with open(filename, "rb") as fr:
-            counter = 1
-            original_filename = filename.split(".")
-            readlimit = self.blocksize  # read 5kb at a time
-            n_splits = filesize // self.blocksize
-            print("splitfile: No of splits required: %s" % str(n_splits))
-            for i in range(n_splits + 1):
-                chunks_count = int(self.blocksize)//int(readlimit)
-                data_chunk = fr.read(readlimit)  # read
-                print(data_chunk)
+        path = path.resolve(strict=False)  # Requires Python 3.6
+        size = path.stat().st_size
+        print("getfilesize: size: %s" % size)
+        return size
 
-                # Create split files
-                print("chunks_count: %d" % chunks_count)
-                with open(original_filename[0]+"_{id}.".format(id=str(counter))+original_filename[1], "ab") as fw:
-                    fw.seek(0)
-                    fw.truncate()  # truncate original if present
-                    while data_chunk:
-                        fw.write(data_chunk)
-                        if chunks_count:
-                            chunks_count -= 1
-                            data_5kb = fr.read(readlimit)
-                        else:
-                            break
-                counter += 1
+    # path: Path object according to https://docs.python.org/zh-cn/3/library/pathlib.html
+    def split_and_hash_file(self, path):
+        if not path.is_file():
+            print("No such file as: \"%s\"" % str(path))
+            return
+
+        path = path.resolve(strict=False)  # Requires Python 3.6
+        filesize = self.get_file_size(path)
+        bytes = path.read_bytes()
+        assert len(bytes) == filesize
+
+        chunks = [bytes[chunk_count * self.blocksize: (chunk_count + 1) * self.blocksize]
+                  for chunk_count in range(-(-len(bytes) // self.blocksize))]
+        hashes = [self.hash_chunk(chunk) for chunk in chunks]
+        return chunks, hashes
+
+    def load_file_info_map(self):
+        index_txt_path = Path(self.basedir / "index.txt")
+        another_kind_of_file = False
+        if index_txt_path.is_symlink():
+            another_kind_of_file = True
+        elif not index_txt_path.exists():
+            index_txt_path.touch(mode=0o644)
+        elif not index_txt_path.is_file():  # it is not a regular file
+            another_kind_of_file = True
+        if another_kind_of_file:
+            if input("Do you want to remove index.txt in order to use the SurfStore client? [Y/N]").lower() == "y":
+                try:
+                    index_txt_path.unlink()  # remove file or symbolic link
+                except Exception:  # Might be a path
+                    try:
+                        shutil.rmtree(str(index_txt_path))  # remove path
+                    except Exception as e:
+                        raise FileNotFoundError(
+                            "Unable to create the required index.txt file!")
+                index_txt_path.touch(mode=0o644, exist_ok=False)
+            else:
+                raise FileNotFoundError(
+                    "Unable to create the required index.txt file!")
+        return index_txt_path.read_text().splitlines()
+
+    def sync(self):
+        try:
+            assert self.basedir.is_dir()
+            assert os.access(str(self.basedir), os.R_OK)
+            assert os.access(str(self.basedir), os.W_OK)
+            assert os.access(str(self.basedir), os.X_OK)
+
+            self.client.surfstore.ping()
+            print("Ping() successful")
+
+        except Exception as e:
+            print("Client sync() init: " + str(e))
+            return
+
+        file_info_map = None
+        try:
+            file_info_map = self.load_file_info_map()  # Read index.txt to memory
+        except Exception as e:
+            print("Client sync() file info map loading: " + str(e))
+            return
+
+        # Get all file names in this directory
+        files = [x for x in self.basedir.iterdir()]
+        for file in files:  # Iterate folder
+            # Open the file only if it is not a directory and is not index.txt
+            if file.name != "index.txt" and file.is_file():
+                # Get the chunks and the SHA-256 hash of each trunk
+                chunks, hashes = self.split_and_hash_file(file)
+
 
 def main():
     # the right format is: python client.py http://localhost:8080 basedir_address 1024(for example)
@@ -96,6 +125,8 @@ def main():
     client = Client(args)
     if client.error:
         exit(1)
+    client.sync()
+
 
 if __name__ == "__main__":
     main()
